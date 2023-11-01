69cfa5383262ba5c42d65f22f6888c8d6dd12567
Wooyun Joh <jeremiahjoh@sungkyul.ac.kr>
2023-11-01 15:25:24 +0900

    Add/Implement stack

--- src/stack.h
+++ src/stack.h
@@ -2,35 +2,21 @@
 #define VAL
 
 #include "value.h"
-
-#endif
+#include <stddef.h>
+#define STACK_SIZE 16
 
 struct stack {
-	struct value *values;	//value의 가변 길이 배열
-	int size;				//스택 안에 있는 값들의 개수
+	struct value value[STACK_SIZE];
+	size_t top;
 };
 
-/*
- *  values 가변 배열 끝에 새로운 value 이어붙이기
- */
-void
+int
 stack_push(struct stack *, struct value);
 
-/*
- * 	만약 스택에 값이 있다면
- * 		values 가변 배열 마지막에 있는 값을 삭제 후 struct value *에 대입
- * 		0 반환
- * 	아니면
- * 		-1 반환
- */
 int
 stack_pop(struct stack *, struct value *);
 
-/*
- *  만약 스택에 값이 있다면
- *  	values 가변 배열 마지막에 있는 값의 포인터 반환
- *  아니면
- *  	NULL 반환
- */
 struct value *
-stack_last(struct stack *);
+stack_top(struct stack *);
+
+#endif
