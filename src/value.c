90a395a2b0ba47928dcd0a2e3ebe91ffa57b9457
sssong5252 <luckyjude@naver.com>
2023-11-01 23:56:41 +0900

    txt pop

--- src/value.c
+++ src/value.c
@@ -63,3 +63,22 @@ value_txt_push(struct value *val, char ch)
     val->data.txt[len] = ch;
     return 0;
 }
+
+int
+value_txt_pop(struct value *val, char *ch)
+{
+    if(val->type != TXT || val->data.txt == NULL)
+        return -1;
+
+    int len = strlen(val->data.txt);
+
+    //문자열이 비어있는 경우 -1 반환
+    if(len == 0)
+        return -1;
+    //문자열 끝에 있는 문자 삭제 후 char *에 대입
+    *ch = val->data.txt[len - 1];
+    val->data.txt[len - 1] = '\0';
+
+    val->data.txt = realloc(val->data.txt, len);
+    return 0;
+}
