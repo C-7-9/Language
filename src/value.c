e899d0b265a66c5ae03dc1e85b4d2e618a3abc03
junhyung001 <junhyung@gmaail.com>
2023-11-02 19:41:57 +0900

    fix: arr remove

--- src/value.c
+++ src/value.c
@@ -80,70 +80,66 @@ value_arr_pop(struct value *val, struct value *pop_value)
 	return 0;	
 }
 
-int value_arr_insert(struct value *val, struct value insert_value, int index)
+
+
+/*
+ * 	문자 삽입
+ *
+ * 	만약 int가 문자열 인덱스 범위 안에 있다면
+ * 		문자열에 char를 끼워넣기
+ * 		0 반환
+ * 		아니면
+ * 		-1 반환
+ */
+int 
+value_arr_insert(struct value *val, struct value insert_value, int index)
 {
-    // 자료형 타입이 arr인지 확인한다.
     if (val->type != ARR)
         return -1;
 
-    // 삽입할 위치가 유효한지 확인한다.
     if (index < 0 || index > val->data.arr.len)
         return -1;
-	
-    // 삽입할 값을 저장한다.
-    val->data.arr.arr_val[index] = insert_value;
 
-    return 0;
-}
-
-int value_arr_insert(struct value *val, struct value insert_value, int position)
-{
-    // 자료형 타입이 arr인지 NULL인지 확인한다.
-    if (val->type != ARR || val->data.arr.arr_val == NULL)
-        return -1;
-
-    // 유효한 삽입 위치를 확인합니다.
-    if (position < 0 || position > val->data.arr.len)
-        return -1;
-
-    // 삽입 위치를 기준으로 배열을 확장합니다.
     val->data.arr.arr_val = realloc(val->data.arr.arr_val, (val->data.arr.len + 1) * sizeof(struct value));
 
-    if (val->data.arr.arr_val == NULL)
-        return -1;
+    for (int i = val->data.arr.len; i > index; i--)
+        val->data.arr.arr_val[i] = val->data.arr.arr_val[i-1];
 
-    // 배열을 오른쪽으로 이동하여 삽입할 위치 확보
-    for (int i = val->data.arr.len; i > position; i--) {
-        val->data.arr.arr_val[i] = val->data.arr.arr_val[i - 1];
-    }
-
-    // 값을 삽입하고 배열 길이를 증가시킴
-    val->data.arr.arr_val[position] = insert_value;
+    val->data.arr.arr_val[index] = insert_value;
     val->data.arr.len++;
 
     return 0;
 }
 
 
-
-
-
-
-int
-value_arr_remove(struct value *val, struct value remove_value, int index)
-{
-	// 자료형 타입이 arr인지 확인한다.
+/*
+ * 	배열 삭제
+ *
+ * 	만약 배열 안에 인자가 있고, int가 문자열 인덱스 범위 안에 있다면
+ * 		int에 해당하는 인자 삭제 후 struct value *에 대입
+ * 		0 반환
+ * 		아니면
+ * 		-1 반환
+ */
+int 
+value_arr_remove(struct value *val, struct value *remove_value, int index) {
+	
     if (val->type != ARR)
         return -1;
 
-    // 삽입할 위치가 유효한지 확인한다.
-    if (index < 0 || index > val->data.arr.len)
+    if (index < 0 || index >= val->data.arr.len)
         return -1;
-	
-    remove_value = val->data.arr.arr_val[index];
-    
 
-	return 0;
+    *remove_value = val->data.arr.arr_val[index];
+
+    for (int i = index; i < val->data.arr.len - 1; i++) {
+        val->data.arr.arr_val[i] = val->data.arr.arr_val[i + 1];
+    }
+
+    val->data.arr.len--;
+    val->data.arr.arr_val = realloc(val->data.arr.arr_val, (val->data.arr.len + 1) * sizeof(struct value));
+
+    return 0;
 }
 
 int
@@ -151,16 +147,7 @@ value_arr_get(struct value *, struct value *, int);
 
 // 배열 끝에 추가하기
 void
-value_arr_append(struct value *, struct value)
-{
-    // 자료형 타입이 arr인지 NULL인지 확인한다.
-	if (val->type != ARR || val->data.arr.arr_val == NULL)
-        return -1;
-
-    // 유효한 삽입 위치를 확인합니다.
-    if (position < 0 || position > val->data.arr.len)
-        return -1;
-}
+value_arr_append(struct value *, struct value);
 
 
 // 배열 하나가 있으면 주어진 인덱스기준 왼쪽,오른쪽 나누기
