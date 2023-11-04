785ae59ffd1b41d3eb2bc15bd108ee921efb48a4
Jeremiah Woohyun Joh <jeremiahjoh@sungkyul.ac.kr>
2023-11-04 21:03:18 +0900

    Implement memory

---
+++ src/memory.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include "memory.h"
+
+int
+memory_new_variable(struct memory *mem, char *name, struct value var)
+{
+	if (mem->value == NULL)
+		return -1;
+
+	mem->value = realloc(mem->value, mem->len + 1);
+	mem->value[mem->len] = var;
+	strcpy(mem->name[mem->len], name);
+	mem->len++;
+
+	return 0;
+}
+
+int
+memory_get_variable(struct memory *mem, char *name, struct value *value)
+{
+	if (mem->value == NULL || mem->len == 0)
+		return -1;
+	
+	for (size_t i = mem->len - 1; i; i--) {
+		if (!strcmp(mem->name[i], name)) {
+			*value = mem->value[i];
+			return 0;
+		}
+	}
+
+	return -1;
+}
--- src/memory.h
+++ src/memory.h
@@ -2,10 +2,19 @@
 #define VAL
 
 #include "value.h"
-
-#endif
+#include <stddef.h>
+#define NAME_LEN 16
 
 struct memory {
+	char *name[NAME_LEN];
 	struct value *value;
-	int *scopes;
+	size_t len;
 };
+
+int
+memory_new_variable(struct memory *, char *, struct value);
+
+int
+memory_get_variable(struct memory *, char *, struct value);
+
+#endif
